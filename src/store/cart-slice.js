2f73e2cf9f2ce5437a36a9204507c5748ada4aad
İHLASMERT <ihlas1mert@gmail.com>
2022-06-23 12:17:37 +0300

    advanced redux-toolkit

---
+++ src/store/cart-slice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFormCart(state, action) {
+        const id = action.payload;
+        const existingItem = state.items.find(item=>item.id===id)
+        if(existingItem.quantity===1){
+
+        }else{
+            existingItem.quantity--;
+            existingItem.totalPrice=existingItem.totalPrice-existingItem.price
+            state.totalQuantity--;
+        }
+    },
+  },
+});
+
+export const cartActions  = cartSlice.actions
+
+export default cartSlice;
